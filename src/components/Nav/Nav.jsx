a99214994d0ca5295d35ed9af382083513d6ba44
Lauren Burke <lburke522@gmail.com>
2023-02-16 13:04:57 -0500

    basic file formatting and started navbar

---
+++ src/components/Nav/Nav.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Nav.scss';
+
+export default function Nav() {
+  return (
+    <div className='nav'>
+      <div className='left'>left</div>
+      <div className='center'>center</div>
+      <div className='right'>right</div>
+    </div>
+  )
+}
